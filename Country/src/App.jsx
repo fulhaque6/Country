958cabd99468df1b9c9b0740deda03da858e8776
fulhaque6 <141068202+fulhaque6@users.noreply.github.com>
2025-01-13 20:56:27 +0500

    Added routes + setup architecture

--- Country/src/App.jsx
+++ Country/src/App.jsx
@@ -1,5 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Country from "./pages/Country";
+import About from "./pages/About";
+import AppLayout from "./components/Layout/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div className="bg-red-600">App</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
