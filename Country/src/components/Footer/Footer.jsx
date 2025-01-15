5377ea0e082bdb41201a9feb8e6cf998dca550c1
fulhaque6 <141068202+fulhaque6@users.noreply.github.com>
2025-01-15 13:27:46 +0500

    add info to the footer

--- Country/src/components/Footer/Footer.jsx
+++ Country/src/components/Footer/Footer.jsx
@@ -2,14 +2,14 @@ import FooterData from "../../Api/footerData.json";
 import { IoCallSharp } from "react-icons/io5";
 import { MdPlace } from "react-icons/md";
 import { TbMailPlus } from "react-icons/tb";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-
   const footerIcons = {
-    MdPlace: <MdPlace/>,
-    IoCallSharp: <IoCallSharp/>,
-    TbMailPlusL: <TbMailPlus/>
-  }
+    MdPlace: <MdPlace />,
+    IoCallSharp: <IoCallSharp />,
+    TbMailPlusL: <TbMailPlus />,
+  };
 
   return (
     <footer className="footer-section">
@@ -28,16 +28,11 @@ const Footer = () => {
         })}
       </div>
 
-      {/* <div className="copyright-area">
+      <div className="copyright-area">
         <div className="container">
           <div className="grid grid-two-cols">
             <div className="copyright-text">
-              <p>
-                Copyright &copy; 2024, All Right Reserved
-                <NavLink to="https://thapatechnical.shop/" target="_blank">
-                  ThapaTechnical
-                </NavLink>
-              </p>
+              <p>Copyright &copy; 2025, All Right Reserved</p>
             </div>
 
             <div className="footer-menu">
@@ -48,7 +43,7 @@ const Footer = () => {
 
                 <li>
                   <NavLink
-                    to="https://www.instagram.com/thapatechnical/"
+                    to="https://www.linkedin.com/in/fasih-ul-haq-a2b740211/"
                     target="_blank"
                   >
                     Social
@@ -56,20 +51,20 @@ const Footer = () => {
                 </li>
                 <li>
                   <NavLink
-                    to="https://thapatechnical.shop/source-code"
+                    to="https://github.com/fulhaque6/Country"
                     target="_blank"
                   >
                     Source Code
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/contact">Contact</NavLink>
+                  <NavLink to="contact">Contact</NavLink>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </footer>
   );
 };
